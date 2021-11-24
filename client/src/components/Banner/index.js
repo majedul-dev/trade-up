ebce36ed80239c1ce505db983420466b42c21ee6
majedul-dev <majedulislam956@gmail.com>
2021-11-24 23:07:44 +0600

    first commit

---
+++ client/src/components/Banner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Banner = ({ className }) => {
+  return (
+    <div className="banner">
+      <img src="" alt="Banner" className={className} />
+      <img src="" alt="Banner" className={className} />
+    </div>
+  );
+};
+
+export default Banner;
